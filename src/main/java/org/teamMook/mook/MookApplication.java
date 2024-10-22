1ec5a0347faccc5182dcc30b5cdb2efdf0ebec0e
stpark0059 <stpark0509@gmail.com>
2024-10-22 20:53:18 +0900

    Mook Project Start
    Dependency
    - Spring Web
    - Lombok
    - Spring-Data-JPA
    - MySQL Driver

---
+++ src/main/java/org/teamMook/mook/MookApplication.java
@@ -0,0 +1,13 @@
+package org.teamMook.mook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/teamMook/mook/MookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teamMook.mook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
